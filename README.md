efa81214b8111820d161ce4e828978f84215820f
Abe Leininger <95333017+abeleinin@users.noreply.github.com>
2023-12-10 23:40:41 -0500

    elo and simulation scripts

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # B365 Final Project
 
 Predict NFL Post-season performance 
+
+## Dataset
+
+- [NFL Team Statistics 2010 - 2021](https://www.kaggle.com/datasets/shanyachaubey/nfl20102021offdefplayoffteamstats)
+- [NFL Games Statistics 2002 - 2022](https://www.kaggle.com/datasets/cviaxmiwnptr/nfl-team-stats-20022019-espn)
+
+
---
+++ scripts/espn_api.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import requests
+
+# api_url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+
+class NFLScrapper:
+  def __init__(self):
+    pass
+
+  def get_data(self, url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+      raise RuntimeError('API call failed')
+    
+  def create_data(self, url):
+    
+    res = pd.DataFrame(columns={'home_team', 
+                                'away_team',
+                                'week',
+                                'score_home',
+                                'score_away',
+                                'result_home',
+                                'result_away'})
+    
+    bye_week_data = pd.DataFrame(columns={'Team', 'Week'})
+                    
+    data = self.get_data(url)
+
+    for i, event in enumerate(data['items']):
+      print("Week " + str(i+1))
+
+      week_number_url = event['$ref']
+      week_events = self.get_data(week_number_url)
+      if 'teamsOnBye' in week_events.keys():
+        teams = week_events['teamsOnBye']
+        for team_url in teams:
+          team_data = self.get_data(team_url['$ref'])
+          new_row ={'Team': team_data['displayName'], 
+                    'Week': i+1}
+          bye_week_data = bye_week_data.append(new_row, ignore_index=True)
+      matchups_url = week_events['events']
+      data = self.get_data(matchups_url['$ref'])
+
+      # TODO: Add bye week teams
+      
+      for item in data['items']:
+        game = self.get_data(item['$ref'])
+        teams = game['name'].split(' at ')
+        away_team, home_team = teams
+        print(home_team + " vs. " + away_team)
+
+        comp = game['competitions']
+        team = comp[0]['competitors']
+        
+        for object in team:
+          url = object['score']
+          data = self.get_data(url['$ref'])
+          labs = data.keys()
+          if object['homeAway'] == 'home':
+            if 'winner' in labs:
+              home_team_score = data['displayValue']
+              home_result = data['winner']
+            else:
+              home_team_score = None
+              home_result = None
+          else:
+            if 'winner' in labs:
+              away_team_score = data['displayValue']
+              away_result = data['winner']
+            else:
+              away_team_score = None
+              away_result = None
+        
+        new_row = {'home_team': home_team, 
+               'away_team': away_team, 
+               'week': i+1,
+               'score_home': home_team_score, 
+               'score_away': away_team_score, 
+               'result_home': home_result, 
+               'result_away': away_result}
+
+        res = res.append(new_row, ignore_index=True)
+      
+    res.to_csv('../data/nfl_scrapped_2023.csv', sep=',')
+    bye_week_data.to_csv('../data/nfl_byeweek_2023.csv', sep=',')
+
+if __name__ == '__main__':
+  url = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2023/types/2/weeks'
+
+  nfl = NFLScrapper()
+
+  nfl.create_data(url)
